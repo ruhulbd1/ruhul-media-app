4251b0c2b5b772a2c246c43dce0c7fd147d010db
ruhulbd1 <ruhuaminruhulamin575@gmail.com>
2026-09-12 16:29:38 +0300

    Clean up build.gradle.kts by removing configurations
    Removed Android configuration and dependencies from build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,5 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.aistudio.youtube.cvyqmp"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.aistudio.youtube.cvyqmp"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
